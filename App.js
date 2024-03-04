a2776f1da8f59e6f830ff164e3843f00fa38d22d
MIN HYEOK JEON <mari394337@gmail.com>
2024-03-05 01:22:59 +0900

    third commit

--- App.js
+++ App.js
@@ -63,7 +63,7 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <Text>goodgood</Text>
+      <Text>goodgooddf</Text>
       <LinearGradient
         colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
